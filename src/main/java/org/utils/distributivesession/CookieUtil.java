6d05aea2396fea4c459650cb108860616dc01845
zhangqiming <870987429@qq.com>
2019-08-03 19:01:49 +0800

    util

--- src/main/java/org/utils/distributivesession/CookieUtil.java
+++ src/main/java/org/utils/distributivesession/CookieUtil.java
@@ -17,12 +17,13 @@ public class CookieUtil {
         response.addCookie(cookie);
     }
 
+
     public static String readCookie(String key, HttpServletRequest request) {
         //从请求中拿到所有cookies
         Cookie[] cookies = request.getCookies();
         //如果cookies没有值返回空
         if (cookies != null && cookies.length > 0) {
-            //便利所有cookie 找到自己想要的
+            //遍历所有cookie 找到自己想要的
             for (Cookie cookie : cookies) {
                 if (key.equals(cookie.getName())) {
                     return cookie.getValue();
